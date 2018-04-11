ad707d50db16c184ed2602b1ac02722f0f665a65
Scott Spiers <scott.m.spiers@gmail.com>
2018-04-11 19:47:39 +0100

    Logging input control values to console

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("X Axis Input: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("Y Axis Input: " + CrossPlatformInputManager.GetAxis("Vertical"));
+
+        if (CrossPlatformInputManager.GetButton("Fire1"))
+            Debug.Log("Fire button pressed");
+
+        if (CrossPlatformInputManager.GetButton("Jump"))
+            Debug.Log("Jump button pressed");
+
+        if (CrossPlatformInputManager.GetButton("Crouch"))
+            Debug.Log("Crouch button pressed");
+	}
+}
