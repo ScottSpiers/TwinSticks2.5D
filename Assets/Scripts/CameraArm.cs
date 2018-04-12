9e54312645babb013c8d02ecf3fcb5f99953ccca
Scott Spiers <scott.m.spiers@gmail.com>
2018-04-12 23:46:46 +0100

    Implementing camera panning using right thumbstick by using a camera arm

---
+++ Assets/Scripts/CameraArm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraArm : MonoBehaviour {
+
+    [SerializeField] private float rotationSpeed;
+    private GameObject player;
+    private Vector3 armRot;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        armRot = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        armRot.x += (Input.GetAxis("RVertical") * rotationSpeed);
+        armRot.y += (Input.GetAxis("RHorizontal") * rotationSpeed);
+
+        transform.position = player.transform.position;
+        transform.rotation = Quaternion.Euler(armRot);
+	}
+}
