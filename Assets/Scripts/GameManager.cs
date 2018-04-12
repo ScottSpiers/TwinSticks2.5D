e330853e8a9470ee1cff7138bb1e2d2d0ae30b20
Scott Spiers <scott.m.spiers@gmail.com>
2018-04-12 21:23:46 +0100

    Implementing Pausing using P key and via OnApplicationPause

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,15 +6,25 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
 
     public bool bIsRecording = true;
+    private bool bIsPaused;
 
-	// Use this for initialization
-	void Start ()
+    private float fixedDeltaTime;
+
+    private void Awake()
+    {
+        fixedDeltaTime = Time.fixedDeltaTime;
+        Debug.Log("Awake Called");
+    }
+    // Use this for initialization
+    void Start ()
     {
         PlayerPrefsManager.UnlockLevel(0);
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(0));
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(1));
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(2));
-        bIsRecording = true;	
+        bIsRecording = true;
+        bIsPaused = false;
+        Debug.Log("Start Called");
 	}
 	
 	// Update is called once per frame
@@ -29,5 +39,45 @@ public class GameManager : MonoBehaviour {
         {
             bIsRecording = true;
         }
-	}
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (bIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        bIsPaused = true;
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+    }
+
+    private void Resume()
+    {
+        bIsPaused = false;
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = fixedDeltaTime;
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        bIsPaused = !bIsPaused;
+        if (bIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+        Debug.Log("OnAppPause Called");
+    }
 }
