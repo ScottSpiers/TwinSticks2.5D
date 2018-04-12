8b8f019783d09546753072afad4b0658c4259272
Scott Spiers <scott.m.spiers@gmail.com>
2018-04-12 18:27:39 +0100

    Adding Mobile Joystick & Improving playback.

--- Assets/ReplaySystem.cs
+++ Assets/ReplaySystem.cs
@@ -4,38 +4,78 @@ using UnityEngine;
 
 public class ReplaySystem : MonoBehaviour {
 
-    private const int BUFFER_SIZE = 100;
+    private const int BUFFER_SIZE = 1000;
     private MyKeyFrame[] keyframes = new MyKeyFrame[BUFFER_SIZE];
     private Rigidbody rigidBody;
     private GameManager gm;
+    private bool recToggle;
+    private int startFrame;
+    private int totalFrames;
+    [SerializeField] private int frameCount;
 
 	// Use this for initialization
 	void Start ()
-    {gm = GameObject.FindObjectOfType<GameManager>();
-        rigidBody = GetComponent<Rigidbody>();	
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+        rigidBody = GetComponent<Rigidbody>();
+        recToggle = !gm.bIsRecording;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         if (gm.bIsRecording)
+        {
             Record();
+        }
         else
+        {
             Playback();
+        }
 	}
 
     void Playback()
     {
-        rigidBody.isKinematic = true;
-        int frame = Time.frameCount % BUFFER_SIZE;
+
+        if(recToggle != gm.bIsRecording)
+        {
+            recToggle = gm.bIsRecording;
+            rigidBody.isKinematic = true;
+
+            int endFrame = Time.frameCount;
+            if((endFrame - startFrame) >= BUFFER_SIZE)
+            {
+                startFrame = endFrame + 1;
+                totalFrames = BUFFER_SIZE;
+            }
+            else
+            {
+                totalFrames = endFrame - startFrame;
+            }
+            frameCount = 0;
+
+        }
+
+        int frame = (startFrame + frameCount) % BUFFER_SIZE;
         Debug.Log("Reading frame: " + frame);
         transform.position = keyframes[frame].pos;
         transform.rotation = keyframes[frame].rot;
+
+        ++frameCount;
+        if (frameCount >= totalFrames)
+        {
+            frameCount = 0;
+        }
     }
 
     void Record()
     {
-        rigidBody.isKinematic = false;
+        if(recToggle != gm.bIsRecording)
+        {
+            recToggle = gm.bIsRecording;
+            rigidBody.isKinematic = false;
+            startFrame = Time.frameCount;
+        }
         int frame = Time.frameCount % BUFFER_SIZE;
         Debug.Log("Writing frame: " + frame);
         keyframes[frame] = new MyKeyFrame(Time.time, transform.position, transform.rotation);
